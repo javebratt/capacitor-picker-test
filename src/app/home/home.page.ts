e59c077294374dcbd6eaec97faa0189604038023
Jorge Vergara <jorverga@cisco.com>
2023-07-26 13:57:04 -0400

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraPluginPermissions, GalleryPhotos } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+images: string[] = [];
   constructor() {}
+
+  pickPhoto = async () => {
+    let images: GalleryPhotos;
+
+    const permissions = await Camera.requestPermissions({
+      permissions: ['photos']
+    });
+
+    if (permissions.photos === 'granted') {
+      images = await Camera.pickImages({
+        quality: 100,
+      });
+    } else {
+      images = await Camera.pickLimitedLibraryPhotos();
+    }
+
+    console.log(images)
+  };
 
 }
